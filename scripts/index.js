b368e1adb91a82bb2abeb91cb8cdeab7f612fbb4
demiz99 <demiz99@mail.ru>
2022-04-20 00:12:39 +0500

    added js logic and css styles

--- scripts/index.js
+++ scripts/index.js
@@ -49,6 +49,81 @@ const initialCards = [
   },
 ];
 
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__save');
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__save_inactive'); 
+  } else {
+    buttonElement.classList.remove('popup__save_inactive');
+  }
+}
+
+const enableValidation = () => {
+  formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+  formElement.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+  });
+
+    setEventListeners(formElement);
+
+    // const fieldsetList = Array.from(formElement.querySelectorAll('.form__set'));
+    // fieldsetList.forEach((fieldSet) => {
+    //   setEventListeners(fieldSet);
+    // });
+
+
+});
+}
+
+enableValidation()
+
+
+function keyHandler (evt, popup) {
+  if (evt.key === "Escape") {
+    closePopup(popup);
+  }
+};
+
 function openPopup(popup) {
   popup.classList.add("popup_open");
 }
@@ -73,6 +148,9 @@ popups.forEach((popup) => {
       closePopup(popup);
     }
     if (evt.target === evt.currentTarget) {
+      closePopup(popup);
+    }
+    if (evt.target.code === 'Escape') {
       closePopup(popup);
     }
   });
